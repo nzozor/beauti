19bbedbf000f65b619089cccedf9023b74941218
nick.z <nzozor@gmail.com>
2019-07-05 19:37:55 +0100

    add sticky to the header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,9 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'beauti-frontend';
-  stickyHeader = false;
-
-  setStickyHeader(value: boolean): void {
-    this.stickyHeader = value;
-  }
 }

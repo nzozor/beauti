a8f90ed1e8290ffe11ab922e9f1ad705cf51285d
nick.z <nzozor@gmail.com>
2019-10-08 22:55:16 +0100

    new map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var require: any;
 
 @Component({
   selector: 'app-map',
@@ -13,13 +14,10 @@ export class MapComponent implements OnInit {
 
   ngOnInit() {
     const mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
-
     mapboxgl.accessToken = 'pk.eyJ1IjoiZWxzYWJlbiIsImEiOiJjanZ4b2ZndDQwNnB5M3pyejNrZWQwaGVwIn0.T8MZoM6PJVvNkME819rAkw';
     const map = new mapboxgl.Map({
       container: 'map',
       style: 'mapbox://styles/elsaben/cjz7dkzjk0txn1co6uw3ktw0m'
     });
   }
-
-
 }
